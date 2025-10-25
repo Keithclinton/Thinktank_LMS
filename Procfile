99b9c1155e47b0e9ecfd55ea01a4f4ad62ab36a7
Keithclinton <keithnyaburi@gmail.com>
2025-10-25 20:31:10 +0300

    Updating the repository to the latest changes

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn thinktank.wsgi
--- courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Course, Note, Video, Assignment
-
-admin.site.register(Course)
-admin.site.register(Note)
-admin.site.register(Video)
-admin.site.register(Assignment)
